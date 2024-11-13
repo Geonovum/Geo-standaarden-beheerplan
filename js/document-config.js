1fa822bda5e7f052aa8362ce2c3ac2f2c62201e9
Gerard Wolbers <g.wolbers@geonovum.nl>
2024-11-13 09:45:08 +0100

    Initial commit

---
+++ js/document-config.js
@@ -0,0 +1,57 @@
+var documentConfig =
+{
+  title: "Beheerplan geo-standaarden",
+  subtitle: "Beheer van de basisset geo-standaarden",
+  shortName: "gsb",
+  pubDomain: "gbd",
+  specStatus: "wv",
+  specType: "bd",
+  license: "cc-by",
+  latestVersion: [
+    "https://docs.geostandaarden.nl/gbd/gsb/"
+  ],
+  publishDate: "2024-11-12",
+  publishVersion: "2.0.0",
+  thisVersion: [
+    "https://docs.geostandaarden.nl/gbd/gsb/"
+  ],
+  previousPublishDate: "2021-08-18",
+  previousPublishVersion: "1.3.1",
+  prevVersion: [
+    "https://docs.geostandaarden.nl/geen/def-bd-BeheerplanGeoStandaarden-20210818/"
+  ],
+  previousMaturity: "def",
+  github: "https://github.com/Geonovum/Geo-standaarden-beheerplan/",
+  issueBase: "https://github.com/Geonovum/Geo-standaarden-beheerplan/",
+  edDraftURI: "https://geonovum.github.io/Geo-standaarden-beheerplan//",
+  canonicalURI: "https://geonovum.github.io/Geo-standaarden-beheerplan//",
+  authors: [
+    {
+      name: "Monique van Scherpenzeel",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    },
+    {
+      name: "Dick Krijtenburg",
+      company: "Geonovum",
+      companyURL: ""
+    }
+  ],
+  editors: [
+    {
+      name: "Monique van Scherpenzeel",
+      company: "Geonovum",
+      companyURL: "https://www.geonovum.nl/"
+    },
+	{
+      name:       "Dick Krijtenburg",
+      company:    "Geonovum",
+      companyURL: "https://www.geonovum.nl"
+    }, 
+	{
+	  name:       "Friso Penninga",
+      company:    "Geonovum",
+      companyURL: "https://www.geonovum.nl"
+    } 
+  ]
+}
